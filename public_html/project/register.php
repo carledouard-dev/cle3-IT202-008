c79012a699949fe2d86c27e7b2583ea8c2f3258d
carledouard-dev <cle3@njit.edu>
2025-03-05 17:48:12 -0500

    Finalized helper functions

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -1,5 +1,5 @@
 <?php
-require(__DIR__ . "/../../lib/functions.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
@@ -37,8 +37,8 @@ if (
     // TODO 3: validate/use
     $hasError = false;
     // Sanitize and validate email
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $email = sanitize_email($email);
+    if (!is_valid_email($email)) {
         echo "Invalid email address";
         $hasError = true;
     }
@@ -68,7 +68,21 @@ if (
     }
 
     if (!$hasError) {
-        echo "Welcome, $email";
+        //echo "Welcome, $email";
+        // TODO 4
+        // comment out the "welcome" line above
+        // TODO 4: Hash password before storing
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $db = getDB(); // available due to the `require()` of `functions.php`
+        // Code for inserting user data into the database
+        $stmt = $db->prepare("INSERT INTO `Users` (email, password) VALUES (:email, :password)");
+        try {
+            $stmt->execute([':email' => $email, ':password' => $hashed_password]);
+            echo "Successfully registered!";
+        } catch (Exception $e) {
+            echo "There was an error registering<br>";
+            echo "<pre>" . var_export($e, true) . "</pre>";
+        }
     }
 }
 ?>
