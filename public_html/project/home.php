57521dc5de3bfb8e799be45da34d73c8a44f4a1c
carledouard-dev <cle3@njit.edu>
2025-03-25 23:02:45 -0400

    Updating stuff and flash stuff

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -1,22 +1,23 @@
 <?php
-require(__DIR__."/../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
 
 //Keep error log for Milestone1 evidence
 //Do not delete
-error_log("Session: ". var_export($_SESSION, true));
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- echo "Welcome, " . $_SESSION["user"]["email"]; 
+error_log("Session: " . var_export($_SESSION, true));
+/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+ flash("Welcome, " . $_SESSION["user"]["email"];")
 }
 else{
-  echo "You're not logged in";
+  flash("You're not logged in");
 }
-if(is_logged_in()){
-    echo "Welcome " . get_user_email();
-}
-else{
-    echo "You're not logged in";
+  */
+if (is_logged_in()) {
+  // flash("Welcome " . get_user_email(");
+} else {
+  flash("You're not logged in");
 }
 ?>
+<?php require_once(__DIR__ . "/../../partials/flash.php");
