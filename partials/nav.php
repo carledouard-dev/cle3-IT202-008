1abe9259870f07cc3895adf721d6852810c09e59
carledouard-dev <cle3@njit.edu>
2025-04-06 17:47:11 -0400

    Trying again

--- partials/nav.php
+++ partials/nav.php
@@ -37,6 +37,7 @@ session_start();
             <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
             <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
         <?php endif; ?>
+        <!-- Admin section -->
         <?php if (has_role("Admin")) : ?>
             <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
             <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
